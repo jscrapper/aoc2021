fa1950309e63ce1a64838fb37156464b2f38dff4
jscrapper <jscrapper@fxville.com>
2021-12-06 17:17:05 -0800

    Add a few options to the base Day class: - 'test' will use the results from get_test_input instead of the standard puzzle input. - 'profile' will print how long each part took to execute when using Day.run()

--- aoc2021.py
+++ aoc2021.py
@@ -1,9 +1,12 @@
 import os
+from contextlib import contextmanager
+from time import perf_counter
 
 class Day(object):
-    def __init__(self):
-        self.raw_puzzle_input = self.get_input()
+    def __init__(self, test=False, profile=False):
+        self.raw_puzzle_input = self.get_test_input() if test else self.get_input()
         self.puzzle_input = self.parse_input()
+        self.profile = profile
 
     def get_input(self):
         """ Gets the puzzle input from a text file in the same folder, with a name matching the classname, and the extension '.txt' """
@@ -11,6 +14,9 @@ class Day(object):
         with open(filename, 'r') as f:
             raw_puzzle_input = f.read().splitlines()
         return raw_puzzle_input
+
+    def get_test_input(self):
+        return []
 
     def parse_input(self):
         return self.raw_puzzle_input
@@ -22,5 +28,26 @@ class Day(object):
         raise NotImplementedError
 
     def run(self):
-        print(f"A: {self.a()}")
-        print(f"B: {self.b()}")
+        with timer() as t:
+            a_result = self.a()
+        
+        msg = f"A: {a_result}"
+        if self.profile:
+            msg += f" Completed in {t.time:.4f}s."
+        print(msg)
+
+        with timer() as t:
+            b_result = self.b()
+        
+        msg = f"A: {b_result}"
+        if self.profile:
+            msg += f" Completed in {t.time:.4f}s."
+        print(msg)
+
+class timer:
+    def __enter__(self):
+        self.time = perf_counter()
+        return self
+    
+    def __exit__(self, type, value, traceback):
+        self.time = perf_counter() - self.time
